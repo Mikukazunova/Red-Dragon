b59ae519b5afebeb1e093682df4b6c306fe5cc1c
Mikukazunova <112575325+Mikukazunova@users.noreply.github.com>
2023-08-29 19:50:13 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -30,7 +30,7 @@ global.doc6 = 'application/vnd.android.package-archive'
 
 //owner v card
 global.vcardowner = ['+22654540141'] // owner number
-global.ownername = "Nova T Destiny" // owner name
+global.ownername = "Nova T. Destiny" // owner name
 global.ytname = "" // yt chanel name
 global.socialm = "GitHub: prabhasha2006" // github or insta name
 global.location = "Décadence" // location
